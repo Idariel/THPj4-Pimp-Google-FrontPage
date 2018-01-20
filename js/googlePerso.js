70d6f6529caff3bda62987dc017728e41432fe7d
Isabelle Lechat <Idariel@users.noreply.github.com>
2018-01-20 12:23:12 +0100

    Update googlePerso.js

--- js/googlePerso.js
+++ js/googlePerso.js
@@ -1,16 +1,41 @@
-$('#main').hide();
-$('.logoMain').hide();
-$('.centerDiv').hide();
-$('.buttons').hide();
-$('.boutonGris').hide();
+window.addEventListener('DOMContentLoaded', function(){
+  "use strict";
+  // $('#indexIndex').hide();
+  // setTimeout(function(){
+  //   $('#indexIndex').fadeIn(4000);
+  // }, 1000);
 
-$('#grosBouton').on('click', fonction (){
+  //Seul le gros bouton Vert "Afficher les éléments centraux" apparaît
+  $('#main').hide();
+  $('.logoMain').hide();
+  $('.centerDiv').hide();
+  $('.buttons').hide();
+  $('.boutonGris').hide();
+
+  // Apparition du bouton vert
   $('#grosBouton').hide();
-  $('#main').fadeIn(2000);
-  $('.logoMain').fadeIn(2000);
-  $('.centerDiv').fadeIn(2000);
-  $('.buttons').fadeIn(2000);
-  $('.boutonGris').fadeIn(2000);
-  $('#grosBouton').width = 0;
-  $('#grosBouton').height = 0;
-});
+  setTimeout(function(){
+    $('#grosBouton').fadeIn(2000);
+  }, 500);
+
+
+  // Faire apparaître les éléments centraux quand le gros bouton vert est cliqué
+  document.getElementById("grosBouton").addEventListener("click", function( event ) {
+    $('#grosBouton').hide();
+    $('#main').show();
+    $('.logoMain').fadeIn(500);
+    setInterval(function(){}, 500);
+    $('.centerDiv').show(1000);
+    setInterval(function(){
+    $('.buttons').show();
+    $('.boutonGris').fadeIn(3000);
+  }, 500);
+  
+  }, false);
+
+
+// setInterval(function(){ alert("Hello"); }, 3000);
+
+
+
+}); // Fin de window.addEventListener('DOMContentLoaded'
